1dcf9f35cb34d297f32fc7a0c86277b15386419a
Ridho Octanio Noya <ridho@crocodic.com>
2022-01-14 09:40:22 +0700

    Initial Commit

---
+++ TemplatePelatihanIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TemplatePelatihanIOS
+//
+//  Created by MBP2017 i7 on 14/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
